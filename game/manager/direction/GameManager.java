b46a1f38c4add1a36cb1e6d95569b391c28be113
noman sardar <nsardar@studenti.laziodigital.it>
2024-05-09 13:21:27 +0200

    reinitialized the GameManeger class

--- game/manager/direction/GameManager.java
+++ game/manager/direction/GameManager.java
@@ -1,19 +1,31 @@
 package game.manager.direction;
 
+import game.manager.Player;
 import game.manager.Room;
 
 public class GameManager {
     private Room room;
+    private Player player;
 
-    public GameManager() {
-    }
-
-    public void setCurrentRoom(Room room) {
+    public GameManager(Room room, Player player) {
         this.room = room;
+        this.player = player;
     }
 
-    public Room getCurrentRoom() {
+    //getters and setters
+    public Room getRoom() {
         return room;
     }
 
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }

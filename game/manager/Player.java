f38cb8631d0399b34cc04e3c181e3e8d08ab0f5a
noman sardar <nsardar@studenti.laziodigital.it>
2024-05-09 14:59:56 +0200

    added a new class InputControl

--- game/manager/Player.java
+++ game/manager/Player.java
@@ -27,12 +27,6 @@ public class Player {
         this.lifePoints = lifePoints;
     }
 
-    //gets user input
-    public static String initializePlayer(){
-        Scanner scr=new Scanner(System.in);
-        String player= scr.nextLine();
-        System.out.println("Hi player! welcome to the game, what is your name? " + player );
-        return player;
-    }
+
 
 }
